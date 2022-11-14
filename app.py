97c399f433a465b1809e7b57f4f467340a0b9392
Jeisen515 <jeisen.soler@upr.edu>
2022-11-13 22:13:18 -0400

    Added a registration page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from Handler.User import UserHandler
 
 from flask_cors import CORS, cross_origin
@@ -9,6 +9,24 @@ CORS(app)
 @app.route('/')
 def greeting():
     return "Los Caballotes de la Universidad"
+
+@app.route('/register')
+def registerUser():
+    return send_file("register.html")
+
+@app.route('/register/verification', methods=["POST"])
+def verifyRegistration():
+    first_name = request.form['user_fname']
+    last_name = request.form['user_lname']
+    password = request.form['user_password']
+    date_of_birth = request.form['user_dob']
+    email = request.form['new_email']
+    phone_number = request.form['user_pnumber']
+
+    #Este return es para que vean que los parametros pasaron del form de la pagina aqui
+    #Lo siguiente seria crear una forma de verificar el email con los emails de la base de datos
+    #Este codigo no es perfecto pero lo hice como en una hora xD
+    return first_name + last_name + password + date_of_birth + email + phone_number
 
 @app.route('/users', methods=['GET', 'POST'])
 def getAllUsers():
