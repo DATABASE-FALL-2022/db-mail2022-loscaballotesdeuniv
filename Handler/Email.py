78d53efc8efd45a0aa841f16fee7ffe912c97d6b
JoseCrespo <jose.crespo14@upr.edu>
2022-11-19 21:25:34 -0400

    Create Email, insert into folder table

--- Handler/Email.py
+++ Handler/Email.py
@@ -1,6 +1,6 @@
 from flask import jsonify
 from DAO.Email import EmailDao
-
+from DAO.Folder import FolderDao
 
 class EmailHandler:
 
@@ -16,6 +16,17 @@ class EmailHandler:
         result['recipientid'] = row[7]
         return result
 
+    def build_email_attributes(self, user_id, eid, ename, subject, body, emailtype, isread, recipientid):
+        result = {}
+        result['user_id'] = user_id
+        result['eid'] = eid
+        result['ename'] = ename
+        result['subject'] = subject
+        result['body'] = body
+        result['emailtype'] = emailtype
+        result['isread'] = isread
+        result['recipientid'] = recipientid
+        return result
 
     def getAllUsersEmails(self):
         dao = EmailDao()
@@ -56,3 +67,29 @@ class EmailHandler:
 
 
 
+    def createEmailJson(self, json):
+        user_id = json['user_id']
+        ename = json['ename']
+        subject = json['subject']
+        body = json['body']
+        emailtype = json['emailtype']
+        isread = json['isread']
+        recipientid = json['recipientid']
+
+        if user_id and ename and subject and body and emailtype and isread and recipientid:
+            edao = EmailDao()
+            fdao = FolderDao()
+            eid = edao.insertNewEmail(user_id, ename, subject, body, emailtype, isread, recipientid)
+            result = self.build_email_attributes(eid, user_id, ename, subject, body, emailtype, isread, recipientid)
+
+            wasdeleted = False
+            draft = "Draft"
+
+            fcheck = fdao.insertIntoFolder(user_id, eid, draft, wasdeleted)
+
+            if fcheck:
+                return jsonify(Email=result), 201
+            else:
+                return jsonify(Error="Unexpected attributes in post request"), 400
+        else:
+            return jsonify(Error="Unexpected attributes in post request"), 400
--- Handler/Folder.py
+++ Handler/Folder.py
@@ -12,6 +12,15 @@ class FolderHandler:
         result['wasdeleted'] = row[3]
         return result
 
+
+    def build_folder_attributes(self, user_id, eid, folder_name, wasdeleted):
+        result = {}
+        result['user_id'] = user_id
+        result['eid'] = eid
+        result['folder_name'] = folder_name
+        result['wasdeleted'] = wasdeleted
+        return result
+
     def getAllFolders(self):
         dao = FolderDao()
         folder_list = dao.getAllFolders()
@@ -21,3 +30,22 @@ class FolderHandler:
             result_list.append(result)
         return jsonify(Folders=result_list)
 
+
+
+
+    def insertFolderJson(self, json):
+        user_id = json['user_id']
+        eid = json['eid']
+        folder_name = json['folder_name']
+        wasdeleted = json['wasdeleted']
+
+        if user_id and eid and folder_name and wasdeleted:
+            dao = FolderDao()
+            result = self.build_folder_attributes(user_id, eid, folder_name, wasdeleted)
+            check = dao.insertIntoFolder(user_id, eid, folder_name, wasdeleted)
+            if check:
+                return jsonify(Folder=result), 201
+            else:
+                return jsonify(Error="Unexpected attributes in post request"), 400
+        else:
+            return jsonify(Error="Unexpected attributes in post request"), 400
